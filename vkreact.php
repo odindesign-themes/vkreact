620d4f4d2f1a51569ff719c44c3b603186c9e658
odindesign-themes <71028280+odindesign-themes@users.noreply.github.com>
2020-09-21 12:22:03 -0300

    Updates plugin version to 1.0.1

--- vkreact.php
+++ vkreact.php
@@ -3,7 +3,7 @@
  * Plugin Name: Vikinger Reactions
  * Plugin URI: http://odindesign-themes.com/
  * Description: Add reactions to your blog posts and comments.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Odin Design Themes
  * Author URI: https://themeforest.net/user/odin_design
  * License: https://themeforest.net/licenses/
@@ -20,7 +20,7 @@ if (!defined('ABSPATH')) {
 /**
  * Versioning
  */
-define('VKREACT_VERSION', '1.0.0');
+define('VKREACT_VERSION', '1.0.1');
 define('VKREACT_VERSION_OPTION', 'vkreact_version');
 
 /**
